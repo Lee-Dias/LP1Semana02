35d6223ef78e798d5b5c044ab7c58251b9f14209
lee-dias <leedias.dev@gmail.com>
2024-02-29 17:07:40 +0000

    update ValoresEspeciais a imprimir todos os valores maximos e minimos de todos os tipos

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,42 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            sbyte sbmin = sbyte.MinValue;
+            sbyte sbmax = sbyte.MaxValue;
+
+            short shmin = short.MinValue;
+            short shmax = short.MaxValue;
+
+            int inmin = int.MinValue;
+            int inmax = int.MaxValue;
+
+            long lomin = long.MinValue;
+            long lomax = long.MaxValue;
+
+            byte bymin = byte.MinValue;
+            byte bymax = byte.MaxValue;
+
+            ushort usmin = ushort.MinValue;
+            ushort usmax = ushort.MaxValue;
+
+            uint uimin = uint.MinValue;
+            uint uimax = uint.MaxValue;
+
+            ulong ulmin = ulong.MinValue;
+            ulong ulmax = ulong.MaxValue;
+
+            char chmin = char.MinValue;
+            char chmax = char.MaxValue;
+
+            Console.WriteLine($"sbyte max e min values: {sbmin}  {sbmax}");
+            Console.WriteLine($"short max e min values: {shmin}  {shmax}");
+            Console.WriteLine($"int max e min values: {inmin}  {inmax}");
+            Console.WriteLine($"long max e min values: {lomin}  {lomax}");
+            Console.WriteLine($"byte max e min values: {bymin}  {bymax}");
+            Console.WriteLine($"ushort max e min values: {usmin}  {usmax}");
+            Console.WriteLine($"uint max e min values: {uimin}  {uimax}");
+            Console.WriteLine($"ulong max e min values: {ulmin}  {ulmax}");
+            Console.WriteLine($"char max e min values: {chmin}  {chmax}");
         }
     }
 }
