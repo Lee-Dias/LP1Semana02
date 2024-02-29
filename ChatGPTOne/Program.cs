243d07bfbad60c7637dbc8ab7a5413c5393bfc9f
lee-dias <leedias.dev@gmail.com>
2024-02-29 19:20:53 +0000

    add ChatGPTOne asking question to user and user is enabled to exit

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis
+            string question, response;
+
+            // pergunta ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Respostas as perguntas
+            switch (question)
+            {
+                case "EXIT":
+                    response = "";
+                    break;
+                case "Who are you?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
+                    break;
+                default:
+                    response = "I'am sorry but that question i can't answer";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
