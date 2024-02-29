53d51c84332a89b71e6b0f7c871ab16d8369a8b5
lee-dias <leedias.dev@gmail.com>
2024-02-29 19:29:01 +0000

    update ChatGPTOne responding to all four questions in loop and with exit

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -8,36 +8,41 @@ namespace ChatGPTOne
         {
             // Variáveis
             string question, response;
+            bool exit = true;
 
             // pergunta ao utilizador
-            Console.Write("Place your question? ");
-            question = Console.ReadLine();
+            while (exit == true){
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
 
-            // Respostas as perguntas
-            switch (question)
-            {
-                case "EXIT":
-                    response = "";
-                    break;
-                case "Who are you?":
-                    response = "I'am God!";
-                    break;
-                case "How can i be better in school?":
-                    response = "Be more attentive to class and study!";
-                    break;
-                case "Where do u live?":
-                    response = "I don't live in one place I am everywhere!";
-                    break;
-                case "How can i be intelligent like you?":
-                    response = "U can't because I know everything!";
-                    break;
-                default:
-                    response = "I'am sorry but that question i can't answer";
-                    break;
+                // Respostas as perguntas
+                switch (question)
+                {
+                    case "EXIT":
+                        response = "";
+                        exit = false;
+                        break;
+                    case "Who are you?":
+                        response = "I'am God!";
+                        break;
+                    case "How can i be better in school?":
+                        response = "Be more attentive to class and study!";
+                        break;
+                    case "Where do u live?":
+                        response = "I don't live in one place I am everywhere!";
+                        break;
+                    case "How can i be intelligent like you?":
+                        response = "U can't because I know everything!";
+                        break;
+                    default:
+                        response = "I'am sorry but that question i can't answer";
+                        break;
+                }
+
+                // Responder adequadamente
+                Console.WriteLine(response);
             }
 
-            // Responder adequadamente
-            Console.WriteLine(response);
-        }
+            }
     }
 }
