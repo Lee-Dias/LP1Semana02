f4c5c684294219beab0c4fced39a59d3869421d3
lee-dias <leedias.dev@gmail.com>
2024-02-29 16:43:47 +0000

    Add Cilindro com os writelines e os readlines a pedir ao utilizador a altura e o raio para o cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique uma altura para um cilindro: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Indique um raio para um cilindro: ");
+            string r = Console.ReadLine();
+        }
+    }
+}
