2b14706d74d09a19744a2be02daa154cebf73697
lee-dias <leedias.dev@gmail.com>
2024-02-29 16:53:36 +0000

    update Cilindro com o Volume e a superficie do cilindro a serem calculados

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -13,7 +13,9 @@ namespace Cilindro
             float a = float.Parse(area);
             float r = float.Parse(rai);
             float v = MathF.PI*MathF.Pow(r,2)*a;
+            float s = 2*MathF.PI*r*(r+a);
             Console.WriteLine($"Volume do cilindro {v}");
+            Console.WriteLine($"area da superficie do cilindro {s}");
         }
     }
 }
